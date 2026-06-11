049404c6422b292ba45eecf386bd86a8f42549c6
Abhisek753 <properachievement@gmail.com>
2026-06-11 21:28:34 +0530

    my carousal

--- Frontend/JAVASCRIPT/Video_Stream_App/index.js
+++ Frontend/JAVASCRIPT/Video_Stream_App/index.js
@@ -1,10 +1,9 @@
 const carousalImages=["https://img.magnific.com/free-vector/online-cinema-banner-with-open-clapper-board-film-strip_1419-2242.jpg","https://cdn.dribbble.com/userupload/7252257/file/original-2b59b6e3d487636b28446d723a776566.jpg?resize=752x&vertical=center","https://chennaivision.com/tamil-movies/wp-content/uploads/2021/08/cv1.jpg"];
 
 
-const carousalContainer=document.getElementById("carousal-container");
+let currentSlide=0;
 
 function initCarousal(){
-    console.log("my carousal");
     carousalImages.forEach((imageUrl,index)=>{
       const slide=document.createElement("div");
       slide.className="carousal-slide"
@@ -19,4 +18,31 @@ function initCarousal(){
       carousalContainer.appendChild(slide);
     })
 }
+
+function updateCarousal(){
+    const slides=document.querySelectorAll(".carousal-slide");
+   
+    slides.forEach((slide,index)=>{
+        if(index===currentSlide){
+        
+            slide.classList.add("active");
+        }else{
+          slide.classList.remove("active");
+        }
+    })
+}
+
+function autoNext(){
+   currentSlide=(currentSlide+1)%carousalImages.length;
+   updateCarousal()
+}
+
+function changeSlide(direction){
+
+ currentSlide=(currentSlide+direction+carousalImages.length)%carousalImages.length;
+ updateCarousal();
+
+}
+
 initCarousal()
+setInterval(autoNext,2000);
