bcfb68439525b5ab15d5f0bb9de121b5ea08ca4d
Abhisek753 <properachievement@gmail.com>
2026-06-11 22:29:53 +0530

    added card

--- Frontend/JAVASCRIPT/Video_Stream_App/index.js
+++ Frontend/JAVASCRIPT/Video_Stream_App/index.js
@@ -3,6 +3,62 @@ const carousalImages=["https://img.magnific.com/free-vector/online-cinema-banner
 
 const carousalContainer=document.getElementById("carousal-container");
 let currentSlide=0;
+let allMovies=[];
+const moviesContainer=document.getElementById("movies-container");
+
+
+async function getMoviesData(){
+ try{
+     const response=await fetch("http://localhost:3000/movies");
+     allMovies=await response.json();
+     displayMovies(allMovies) ;    
+ }catch(err){
+  console.log(err)
+ }
+}
+
+
+function displayMovies(movies=allMovies){
+
+if(!moviesContainer){
+  console.log("Movies container is missing");
+  return;
+}
+if(!movies || movies.length==0){
+  moviesContainer.innerHTML="<p>No movies found</p>"
+}
+movies.forEach((movie=>{
+ 
+  const card=document.createElement("div");
+  card.className="movie-card";
+  card.innerHTML=`
+  <div class="movie-poster">
+  <img src=${movie.poster} alt=${movie.title} class="movie-poster-img">
+  </div>
+   <div class="movie-info">
+     <div class="movie-title">${movie.title}</div>
+       <div class="movie-year">${movie.year}</div>
+         <div class="movie-category">${movie.Category}</div>
+         <div class="movie-rating">${movie.rating}⭐</div>
+         <div class="movie-buttons">
+         <button class="btn btn-cart">🛒Cart
+         </button>
+          <button class="btn btn-favourite">❤️Favourite
+         </button>
+         </div>
+  </div>
+  
+  `
+  moviesContainer.appendChild(card);
+}))
+
+}
+
+
+
+
+
+
 
 function initCarousal(){
     carousalImages.forEach((imageUrl,index)=>{
@@ -46,6 +102,6 @@ function changeSlide(direction){
  updateCarousal();
 
 }
-
+getMoviesData()
 initCarousal()
 setInterval(autoNext,2000);
